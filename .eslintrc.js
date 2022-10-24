528f8e7e97037d97d66db470e0e5de62e0584f00
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-10-24 16:14:12 +0200

    dev: less strict eslint config for IDE

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+// @ts-check
+const { defineConfig } = require("eslint-define-config");
+
+module.exports = defineConfig({
+    extends: ["./.eslintrc.build.js"],
+    rules: {
+        // better DX
+        "import/no-unused-modules": "off",
+        "unused-imports/no-unused-imports": "off",
+        "simple-import-sort/imports": "off",
+        "@typescript-eslint/consistent-type-imports": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-unused-expressions": "off",
+        "no-empty": "off",
+        "prettier/prettier": "off",
+    },
+});
