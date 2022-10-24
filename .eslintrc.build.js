fcfd05ed3c1cd2ca91d2a93cb9598905751add43
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-10-24 16:14:12 +0200

    dev: less strict eslint config for IDE

---
+++ .eslintrc.build.js
@@ -0,0 +1,27 @@
+// @ts-check
+const { defineConfig } = require("eslint-define-config");
+
+module.exports = defineConfig({
+    parserOptions: {
+        files: ["*.ts"],
+        project: ["./tsconfig.json"],
+    },
+    ignorePatterns: ["**/*.test.ts"],
+    extends: "@astahmer/eslint-config-ts",
+    settings: {
+        "import/extensions": [".ts"],
+    },
+    rules: {
+        // "file-progress/activate": 1
+        "unicorn/prefer-module": 0,
+        "sonarjs/cognitive-complexity": ["warn", 30],
+    },
+    overrides: [
+        {
+            files: ["src/index.ts"],
+            rules: {
+                "import/no-unused-modules": 0,
+            },
+        },
+    ],
+});
