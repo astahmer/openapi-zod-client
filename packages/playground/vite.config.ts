a48ea7e8578d09ed25e1421a9bf5140bad04fb5a
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-10-29 10:49:18 +0200

    fix: server-side package with ssr external + rakkas preview

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -7,39 +7,7 @@ export default defineConfig({
     define: {
         "process.env.TEST": false,
     },
-    build: {
-        commonjsOptions: {
-            transformMixedEsModules: true,
-        },
-        // rollupOptions: {
-        //     external: ["handlebars"],
-        // },
-    },
     ssr: {
-        // format: "cjs",
-        // noExternal: ["handlebars"],
-        // external: ["handlebars"],
-        optimizeDeps: {
-            // exclude: ["handlebars"],
-            // exclude: ["handlebars"],
-            // exclude: ["tanu"],
-            // include: ["handlebars", "tanu"],
-        },
-    },
-    optimizeDeps: {
-        // include: ["tanu"],
-        // exclude: [
-        //     "fs-extra",
-        //     "openapi3-ts",
-        //     // "handlebars",
-        //     "pastable/server",
-        //     // "tanu",
-        //     // "whence",
-        //     // "prettier",
-        //     "yaml",
-        //     "yaml*",
-        //     "yaml@2.1.3",
-        //     "ts-toolbelt",
-        // ],
+        external: ["handlebars", "tanu", "whence"],
     },
 });
