ca734db6e29b72886b98935cd39112969fae3c11
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-11-02 00:57:32 +0100

    feat: export getHandlesbars / maybePretty types: export TemplateContext / TemplateContextOptions

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -3,4 +3,6 @@ export { generateZodClientFromOpenAPI } from "./generateZodClientFromOpenAPI";
 export { getOpenApiDependencyGraph } from "./getOpenApiDependencyGraph";
 export { type EndpointDefinitionWithRefs, getZodiosEndpointDefinitionList } from "./getZodiosEndpointDefinitionList";
 export { getZodSchema } from "./openApiToZod";
-export { getZodClientTemplateContext } from "./template-context";
+export { getHandlebars } from "./getHandlebars";
+export { maybePretty } from "./maybePretty";
+export { getZodClientTemplateContext, type TemplateContext, type TemplateContextOptions } from "./template-context";
