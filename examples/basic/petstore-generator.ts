9501d4b90d40667f5bea395d96743e71b9e323d0
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-11-07 23:26:30 +0100

    fix: examples

--- examples/basic/petstore-generator.ts
+++ examples/basic/petstore-generator.ts
@@ -1,14 +1,14 @@
 import SwaggerParser from "@apidevtools/swagger-parser";
-import { OpenAPIObject } from "openapi3-ts";
+import type { OpenAPIObject } from "openapi3-ts";
 import { resolveConfig } from "prettier";
-import { generateZodClientFromOpenAPI } from "../src/generateZodClientFromOpenAPI";
+import { generateZodClientFromOpenAPI } from "openapi-zod-client";
 
 const main = async () => {
-    const openApiDoc = (await SwaggerParser.parse("./example/petstore.yaml")) as OpenAPIObject;
+    const openApiDoc = (await SwaggerParser.parse("../petstore.yaml")) as OpenAPIObject;
     const prettierConfig = await resolveConfig("./");
     const result = await generateZodClientFromOpenAPI({
         openApiDoc,
-        distPath: "./example/petstore-client.ts",
+        distPath: "./petstore-client.ts",
         prettierConfig,
     });
     console.log(result);
