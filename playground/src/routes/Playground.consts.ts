f90d2863c101bce05cca94146754370e7e0de364
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-11-07 18:52:55 +0100

    fix: presetTemplate checked

--- playground/src/routes/Playground.consts.ts
+++ playground/src/routes/Playground.consts.ts
@@ -1,30 +1,30 @@
 import { OptionsFormValues } from "../components/OptionsForm";
 
-export type PresetTemplate = { name: string; value: string; template: string; options?: Partial<OptionsFormValues> };
+export type PresetTemplate = { name: string; preset: string; template: string; options?: Partial<OptionsFormValues> };
 export const presetTemplateList = [
-    { name: "Default", value: "default", template: "template-default", options: { groupStrategy: "none" } },
-    { name: "Grouped by tag", value: "grouped-tag", template: "template-grouped", options: { groupStrategy: "tag" } },
+    { name: "Default", preset: "default", template: "template-default", options: { groupStrategy: "none" } },
+    { name: "Grouped by tag", preset: "grouped-tag", template: "template-grouped", options: { groupStrategy: "tag" } },
     {
         name: "Grouped by method",
-        value: "grouped-method",
+        preset: "grouped-method",
         template: "template-grouped",
         options: { groupStrategy: "method" },
     },
     {
         name: "Schemas only",
-        value: "schemas-only",
+        preset: "schemas-only",
         template: "template-schemas-only",
         options: { shouldExportAllSchemas: true },
     },
     {
         name: "Grouped by tag (split files)",
-        value: "grouped-tag-file",
+        preset: "grouped-tag-file",
         template: "template-default",
         options: { groupStrategy: "tag-file" },
     },
     {
         name: "Grouped by method (split files)",
-        value: "grouped-method-file",
+        preset: "grouped-method-file",
         template: "template-default",
         options: { groupStrategy: "method-file" },
     },
