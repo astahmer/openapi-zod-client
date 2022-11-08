331e0a801e8d296314dc426c0b49c6e89559eee7
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-11-08 17:42:08 +0100

    feat(playground): prettier schema in editor

--- playground/src/Playground/Playground.consts.ts
+++ playground/src/Playground/Playground.consts.ts
@@ -3,18 +3,18 @@ import type { OptionsFormValues } from "../components/OptionsForm";
 export type PresetTemplate = { name: string; preset: string; template: string; options?: Partial<OptionsFormValues> };
 export const presetTemplateList = [
     { name: "Default", preset: "default", template: "template-default", options: { groupStrategy: "none" } },
+    {
+        name: "Default - Schemas only",
+        preset: "schemas-only",
+        template: "template-schemas-only",
+        options: { groupStrategy: "none", shouldExportAllSchemas: true },
+    },
     { name: "Grouped by tag", preset: "grouped-tag", template: "template-grouped", options: { groupStrategy: "tag" } },
     {
         name: "Grouped by method",
         preset: "grouped-method",
         template: "template-grouped",
         options: { groupStrategy: "method" },
-    },
-    {
-        name: "Schemas only",
-        preset: "schemas-only",
-        template: "template-schemas-only",
-        options: { groupStrategy: "none", shouldExportAllSchemas: true },
     },
     {
         name: "Grouped by tag (split files)",
---
+++ playground/src/macros/get-language-schemas.ts
@@ -0,0 +1,21 @@
+import { parse } from "yaml";
+import { JSONSchema7 } from "json-schema";
+import schemaPrettier from "./schema-prettierrc.json";
+import { AwaitFn } from "pastable";
+
+const getLanguageSchemas = async () => {
+    return {
+        data: {
+            // https://github.com/SchemaStore/schemastore/blob/1d89c46b81e34e0f6ff9914085eb2ba44808145c/src/schemas/json/prettierrc.json
+            prettier: schemaPrettier,
+            // https://github.com/OAI/OpenAPI-Specification/blob/157a4c81ae537ef793b2bee368bc00d88b461de8/schemas/v3.0/schema.yaml
+            // commented since @monaco-editor/react isn't compatible with monaco-yaml
+            // hopefully it will be fixed in the future
+            // openApiV3: (await parse("./schema-openapi-v3.0.yaml")) as Omit<JSONSchema7, "$id"> & { id: string },
+        },
+    };
+};
+
+export default getLanguageSchemas;
+
+export type GetLanguageSchemasData = AwaitFn<typeof getLanguageSchemas>["data"];
