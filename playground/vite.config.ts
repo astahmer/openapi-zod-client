cb626f83efeefbef0f3cb64328062516b59f1b59
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-11-09 10:46:16 +0100

    dev: add rollup-plugin-visualizer

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,44 +1,53 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import rakkas from "rakkasjs/vite-plugin";
 import compileTime from "vite-plugin-compile-time";
 import UnoCSS from "unocss/vite";
 import presetIcons from "@unocss/preset-icons";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // TODO pwa ?
-export default defineConfig((_env) => ({
-    plugins: [
-        UnoCSS({
-            presets: [presetIcons({})],
-        }),
-        rakkas({
-            react: {
-                jsxImportSource: "/src/emotion",
-                babel: {
-                    plugins: ["@emotion/babel-plugin"],
+export default defineConfig((env) => {
+    const config: UserConfig = {
+        plugins: [
+            UnoCSS({
+                presets: [presetIcons({})],
+            }),
+            rakkas({
+                react: {
+                    jsxImportSource: "/src/emotion",
+                    babel: {
+                        plugins: ["@emotion/babel-plugin"],
+                    },
+                },
+            }),
+            compileTime(),
+        ],
+        define: {
+            "process.env.TEST": true,
+        },
+        optimizeDeps: {
+            esbuildOptions: {
+                define: {
+                    global: "globalThis", // for handlebars
+                    // https://github.com/vitejs/vite/discussions/5912#discussioncomment-3895047
                 },
             },
-        }),
-        compileTime(),
-    ],
-    define: {
-        "process.env.TEST": true,
-    },
-    optimizeDeps: {
-        esbuildOptions: {
-            define: {
-                global: "globalThis", // for handlebars
-                // https://github.com/vitejs/vite/discussions/5912#discussioncomment-3895047
+        },
+        ssr: {
+            external: ["handlebars", "tanu", "whence"],
+            // This is required to fix ESM/CJS incompatibilities
+            noExternal: ["@emotion/styled"],
+        },
+        resolve: {
+            alias: {
+                // "openapi-zod-client": path.resolve(__dirname, "../lib"),
             },
         },
-    },
-    ssr: {
-        external: ["handlebars", "tanu", "whence"],
-        // This is required to fix ESM/CJS incompatibilities
-        noExternal: ["@emotion/styled"],
-    },
-    resolve: {
-        alias: {
-            // "openapi-zod-client": path.resolve(__dirname, "../lib"),
-        },
-    },
-}));
+    };
+
+    if (process.env.VIZ && env.command === "build" && !env.ssrBuild) {
+        config.plugins!.push(visualizer({ open: true }) as any);
+    }
+
+    return config;
+});
