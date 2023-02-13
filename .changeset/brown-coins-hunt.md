1b7d69eb20c15f70e402cc80ae4959b094b32ab6
Frederik Eychenié <277455+feychenie@users.noreply.github.com>
2023-02-13 17:08:14 +0000

    fix: default values for "object" type

---
+++ .changeset/brown-coins-hunt.md
@@ -0,0 +1,5 @@
+---
+"openapi-zod-client": patch
+---
+
+Fixes and issue where default values for objects are incorrectly set (or ommited) in the zod schema.
---
+++ lib/tests/object-default-values.test.ts
@@ -0,0 +1,121 @@
+import type { OpenAPIObject } from "openapi3-ts";
+import { expect, test } from "vitest";
+import { generateZodClientFromOpenAPI } from "../src";
+
+// https://github.com/astahmer/openapi-zod-client/issues/61
+test("object-default-values", async () => {
+    const openApiDoc: OpenAPIObject = {
+        openapi: "3.0.0",
+        info: {
+            version: "1.0.0",
+            title: "object default values",
+        },
+        paths: {
+            "/sample": {
+                get: {
+                    parameters: [
+                        {
+                            in: "query",
+                            name: "empty-object",
+                            schema: {
+                                type: "object",
+                                properties: { foo: { type: "string" } },
+                                default: {},
+                            },
+                        },
+                        {
+                            in: "query",
+                            name: "default-object",
+                            schema: {
+                                type: "object",
+                                properties: { foo: { type: "string" } },
+                                default: { foo: "bar" },
+                            },
+                        },
+                        {
+                            in: "query",
+                            name: "ref-object",
+                            schema: {
+                                type: "object",
+                                additionalProperties: { $ref: "#/components/schemas/MyComponent" },
+                                default: { id: 1, name: "foo" },
+                            },
+                        },
+                    ],
+                    responses: {
+                        "200": {
+                            description: "resoponse",
+                        },
+                    },
+                },
+            },
+        },
+        components: {
+            schemas: {
+                MyComponent: {
+                    type: "object",
+                    properties: {
+                        id: {
+                            type: "number",
+                        },
+                        name: {
+                            type: "string",
+                        },
+                    },
+                },
+            },
+        },
+    };
+
+    const output = await generateZodClientFromOpenAPI({ disableWriteToFile: true, openApiDoc });
+    expect(output).toMatchInlineSnapshot(`
+      "import { makeApi, Zodios, type ZodiosOptions } from "@zodios/core";
+      import { z } from "zod";
+
+      const MyComponent = z.object({ id: z.number(), name: z.string() }).partial();
+
+      export const schemas = {
+        MyComponent,
+      };
+      
+      const endpoints = makeApi([
+        {
+          method: "get",
+          path: "/sample",
+          requestFormat: "json",
+          parameters: [
+            {
+              name: "empty-object",
+              type: "Query",
+              schema: z.object({ foo: z.string() }).partial().optional().default({}),
+            },
+            {
+              name: "default-object",
+              type: "Query",
+              schema: z
+                .object({ foo: z.string() })
+                .partial()
+                .optional()
+                .default({ foo: "bar" }),
+            },
+            {
+              name: "ref-object",
+              type: "Query",
+              schema: z
+                .record(MyComponent)
+                .optional()
+                .default({ id: 1, name: "foo" }),
+            },
+          ],
+          response: z.void(),
+        },
+      ]);
+
+      export const api = new Zodios(endpoints);
+
+      export function createApiClient(baseUrl: string, options?: ZodiosOptions) {
+        return new Zodios(baseUrl, endpoints, options);
+      }
+      "
+    `);
+});
