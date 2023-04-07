76e09519fcf39a632c9324e05fdca8a803c181da
Robert <robert.wysocki@sparkbit.pl>
2023-04-07 18:48:22 +0200

    fix: handle `exclusiveMinimum: false`

--- lib/tests/validations.test.ts
+++ lib/tests/validations.test.ts
@@ -22,10 +22,12 @@ test("validations", () => {
                     intWithMin: { type: "integer", minimum: 3 },
                     intWithMax: { type: "integer", maximum: 3 },
                     intWithMinAndMax: { type: "integer", minimum: 3, maximum: 3 },
-                    intWithExclusiveMinBool: { type: "integer", minimum: 3, exclusiveMinimum: true },
+                    intWithExclusiveMinTrue: { type: "integer", minimum: 3, exclusiveMinimum: true },
+                    intWithExclusiveMinFalse: { type: "integer", minimum: 3, exclusiveMinimum: false },
                     intWithExclusiveMin: { type: "integer", exclusiveMinimum: 3 },
-                    intWithExclusiveMaxBool: { type: "integer", maximum: 3, exclusiveMaximum: true },
-                    intWithExclusiveMax: { type: "integer", maximum: 3, exclusiveMaximum: 3 },
+                    intWithExclusiveMaxTrue: { type: "integer", maximum: 3, exclusiveMaximum: true },
+                    intWithExclusiveMaxFalse: { type: "integer", maximum: 3, exclusiveMaximum: false },
+                    intWithExclusiveMax: { type: "integer", exclusiveMaximum: 3 },
                     intWithMultipleOf: { type: "integer", multipleOf: 3 },
                     //
                     bool: { type: "boolean" },
@@ -55,6 +57,6 @@ test("validations", () => {
             },
         })
     ).toMatchInlineSnapshot(
-        '"z.object({ str: z.string(), strWithLength: z.string().min(3).max(3), strWithMin: z.string().min(3), strWithMax: z.string().max(3), strWithPattern: z.string().regex(/^[a-z]+$/), email: z.string().email(), hostname: z.string().url(), url: z.string().url(), uuid: z.string().uuid(), number: z.number(), int: z.number().int(), intWithMin: z.number().int().gte(3), intWithMax: z.number().int().lte(3), intWithMinAndMax: z.number().int().gte(3).lte(3), intWithExclusiveMinBool: z.number().int().gt(3), intWithExclusiveMin: z.number().int().gt(3), intWithExclusiveMaxBool: z.number().int().lt(3), intWithExclusiveMax: z.number().int().lt(3), intWithMultipleOf: z.number().int().multipleOf(3), bool: z.boolean(), array: z.array(z.string()), arrayWithMin: z.array(z.string()).min(3), arrayWithMax: z.array(z.string()).max(3), object: z.object({ str: z.string() }), objectWithRequired: z.object({ str: z.string() }), oneOf: z.union([z.string(), z.number()]), anyOf: z.union([z.union([z.string(), z.number()]), z.array(z.union([z.string(), z.number()]))]), allOf: z.string().and(z.number()), nested: z.record(z.number()) })"'
+        '"z.object({ str: z.string(), strWithLength: z.string().min(3).max(3), strWithMin: z.string().min(3), strWithMax: z.string().max(3), strWithPattern: z.string().regex(/^[a-z]+$/), email: z.string().email(), hostname: z.string().url(), url: z.string().url(), uuid: z.string().uuid(), number: z.number(), int: z.number().int(), intWithMin: z.number().int().gte(3), intWithMax: z.number().int().lte(3), intWithMinAndMax: z.number().int().gte(3).lte(3), intWithExclusiveMinTrue: z.number().int().gt(3), intWithExclusiveMinFalse: z.number().int().gte(3), intWithExclusiveMin: z.number().int().gt(3), intWithExclusiveMaxTrue: z.number().int().lt(3), intWithExclusiveMaxFalse: z.number().int().lte(3), intWithExclusiveMax: z.number().int().lt(3), intWithMultipleOf: z.number().int().multipleOf(3), bool: z.boolean(), array: z.array(z.string()), arrayWithMin: z.array(z.string()).min(3), arrayWithMax: z.array(z.string()).max(3), object: z.object({ str: z.string() }), objectWithRequired: z.object({ str: z.string() }), oneOf: z.union([z.string(), z.number()]), anyOf: z.union([z.union([z.string(), z.number()]), z.array(z.union([z.string(), z.number()]))]), allOf: z.string().and(z.number()), nested: z.record(z.number()) })"'
     );
 });
