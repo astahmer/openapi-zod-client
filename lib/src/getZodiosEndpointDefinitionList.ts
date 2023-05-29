131bee2fb7fbfc465eb430bcf95dbc5e9dbba10a
dosco <832235+dosco@users.noreply.github.com>
2023-05-29 12:38:09 -0700

    Add z.describe support

--- lib/src/getZodiosEndpointDefinitionList.ts
+++ lib/src/getZodiosEndpointDefinitionList.ts
@@ -251,6 +251,9 @@ export const getZodiosEndpointDefinitionList = (doc: OpenAPIObject, options?: Te
                         paramSchema = paramItem.schema;
                     }
 
+                    const description = paramItem.description ?? "";
+                    paramSchema = { ...paramSchema, description };
+
                     // resolve ref if needed, and fallback to default (unknown) value if needed
                     paramSchema = paramSchema
                         ? (isReferenceObject(paramSchema)
