788e7098fc85fe9ffe19c76cad10cef60472b040
Mike Perrone <mike@gable.ai>
2023-08-07 10:32:46 -0700

    add test showing / incorrectly escaped

---
+++ lib/tests/regex-with-escapes.test.ts
@@ -0,0 +1,18 @@
+import { getZodSchema } from "../src/openApiToZod";
+import { test, expect } from "vitest";
+
+test("regex-with-escapes", () => {
+    expect(
+        getZodSchema({schema: {
+            type: "object",
+            properties: {
+                str: { 
+                    type: "string",
+                    pattern: "^\/$"
+                },
+            }
+        }})
+    ).toMatchInlineSnapshot(
+        '"z.object({ str: z.string().regex(/^\/$/) }).partial().passthrough()"'
+    );
+});
