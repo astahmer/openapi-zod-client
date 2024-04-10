c5f91ed41e89a3f87f525a5d8c3dd5b9a23a5949
Douglas Gadêlha <dgadelha@live.com>
2024-04-10 15:48:58 -0300

    feat: schema refiner

---
+++ lib/tests/schema-refiner.test.ts
@@ -0,0 +1,41 @@
+import { isReferenceObject } from "openapi3-ts";
+import { getZodSchema } from "../src/openApiToZod";
+import { test, expect } from "vitest";
+
+test("schema-refiner", () => {
+    expect(
+        getZodSchema({
+            schema: {
+                properties: {
+                    name: {
+                        type: "string",
+                    },
+                    email: {
+                        type: "string",
+                    },
+                },
+            },
+            options: {
+                schemaRefiner(schema) {
+                    if (isReferenceObject(schema) || !schema.properties) {
+                        return schema;
+                    }
+
+                    if (!schema.required && schema.properties) {
+                        for (const key in schema.properties) {
+                            const prop = schema.properties[key];
+
+                            if (!isReferenceObject(prop)) {
+                                prop.nullable = true;
+                            }
+                        }
+                    }
+
+                    return schema;
+                },
+            },
+        })
+    ).toMatchInlineSnapshot(
+        '"z.object({ name: z.string().nullable(), email: z.string().nullable() }).partial().passthrough()"'
+    );
+});
